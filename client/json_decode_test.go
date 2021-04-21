416cab44b7b0d468c5d51b16b77122913bbd43b5
Bachue Zhou <bachue.shu@gmail.com>
2021-04-21 16:24:53 +0800

    now decoding response body as json failure will show original response body content

---
+++ client/json_decode_test.go
@@ -0,0 +1,20 @@
+package client
+
+import (
+	"testing"
+)
+
+func TestJsonDecode(t *testing.T) {
+	var ret struct{}
+	body := `<?xml version="1.0" encoding="UTF-8"?>
+<Error>
+  <Code>NoSuchKey</Code>
+  <Message>The resource you requested does not exist</Message>
+  <Resource>/mybucket/myfoto.jpg</Resource>
+  <RequestId>4442587FB7D0A2F9</RequestId>
+</Error>`
+	err := decodeJsonFromData([]byte(body), &ret)
+	if err.Error() != "invalid character '<' looking for beginning of value: "+body {
+		t.Fatal("unexpected error message", err.Error())
+	}
+}
