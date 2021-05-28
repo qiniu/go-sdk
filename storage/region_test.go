84b7a5b7d570fa69242b0ee7544e48ab12272bc8
yangsen <yang_sen_ysys@163.com>
2021-05-28 16:49:20 +0800

    add set uc host test case

--- storage/region_test.go
+++ storage/region_test.go
@@ -53,3 +53,15 @@ func TestRegionWithNoProtocol(t *testing.T) {
 		t.Fatalf("region1.IovipHost is wrong: %v\v", region1.IovipHost)
 	}
 }
+
+func TestRegionWithSetHost(t *testing.T) {
+	SetUcHost("uc.qbox.me", true)
+	region1, err := GetRegion(testAK, testBucket)
+	if err != nil {
+		t.Fatalf("GetRegion error: %v\n", err)
+	}
+
+	if !strings.HasPrefix(region1.IovipHost, "iovip") || !strings.HasSuffix(region1.IovipHost, ".qbox.me") {
+		t.Fatalf("region1.IovipHost is wrong: %v\v", region1.IovipHost)
+	}
+}
