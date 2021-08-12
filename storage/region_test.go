ceb4cef2380d5697a07c13ac7f73f3cb4d0adcd8
Bachue Zhou <zhourong@qiniu.com>
2021-08-12 19:52:42 +0800

    分割单元测试和集成测试 (#39)
    * split unit tests and integration tests
    
    * fix ci issue
    
    Co-authored-by: Changjun Ji <jinsdu@outlook.com>

--- storage/region_test.go
+++ storage/region_test.go
@@ -1,35 +1,11 @@
+// +build integration
+
 package storage
 
 import (
 	"strings"
 	"testing"
 )
-
-func TestEndpoint(t *testing.T) {
-	type input struct {
-		UseHttps bool
-		Host     string
-	}
-	testInputs := []input{
-		{UseHttps: true, Host: "rs.qiniu.com"},
-		{UseHttps: false, Host: "rs.qiniu.com"},
-		{UseHttps: true, Host: ""},
-		{UseHttps: false, Host: ""},
-		{UseHttps: true, Host: "https://rs.qiniu.com"},
-		{UseHttps: false, Host: "https://rs.qiniu.com"},
-		{UseHttps: false, Host: "http://rs.qiniu.com"},
-	}
-	testWants := []string{"https://rs.qiniu.com", "http://rs.qiniu.com", "", "", "https://rs.qiniu.com",
-		"http://rs.qiniu.com", "http://rs.qiniu.com"}
-
-	for ind, testInput := range testInputs {
-		testGot := endpoint(testInput.UseHttps, testInput.Host)
-		testWant := testWants[ind]
-		if testGot != testWant {
-			t.Fail()
-		}
-	}
-}
 
 func TestRegion(t *testing.T) {
 	region1, err := GetRegion(testAK, testBucket)
