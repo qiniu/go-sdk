e15a7fd637de0277ab679e147dccaea5623912f2
Shen Junzheng <shenjunzheng@gmail.com>
2021-10-26 18:01:36 +0800

    compatible with older versions of go 1.13

--- go.mod
+++ go.mod
@@ -3,11 +3,17 @@ module github.com/qiniu/go-sdk/v7
 go 1.14
 
 require (
-	github.com/go-playground/validator/v10 v10.9.0
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/go-playground/universal-translator v0.18.0 // indirect
+	github.com/go-playground/validator/v10 v10.8.0
+	github.com/kr/pretty v0.3.0 // indirect
 	github.com/qiniu/dyn v1.3.0
-	github.com/stretchr/testify v1.7.0
+	github.com/rogpeppe/go-internal v1.8.0 // indirect
+	github.com/stretchr/testify v1.6.1
 	golang.org/x/crypto v0.0.0-20210921155107-089bfa567519 // indirect
 	golang.org/x/sync v0.0.0-20201207232520-09787c993a3a
 	golang.org/x/sys v0.0.0-20211020174200-9d6173849985 // indirect
 	golang.org/x/text v0.3.7 // indirect
+	gopkg.in/check.v1 v1.0.0-20201130134442-10cb98267c6c // indirect
+	gopkg.in/yaml.v3 v3.0.0-20210107192922-496545a6307b // indirect
 )
--- pili/url.go
+++ pili/url.go
@@ -6,6 +6,7 @@ import (
 	"crypto/sha1"
 	"encoding/base64"
 	"fmt"
+	"math"
 	"net/url"
 	"path/filepath"
 	"reflect"
@@ -194,7 +195,7 @@ func magicCtx(u *url.URL, query url.Values, key string) map[string]interface{} {
 func convertSRTURL(u *url.URL, token string) string {
 	query := u.Query()
 	query.Set("m", "publish")
-	rawQuery := strings.ReplaceAll(query.Encode(), "&", ",")
+	rawQuery := strings.Replace(query.Encode(), "&", ",", -1)
 	if len(token) > 0 {
 		rawQuery += ",token=" + token
 	}
@@ -258,8 +259,48 @@ func isQuery(q interface{}) (url.Values, bool) {
 }
 
 func setQuery(q url.Values, key string, v interface{}) {
-	if !reflect.ValueOf(v).IsZero() {
+	if !isZero(v) {
 		q.Set(key, fmt.Sprint(v))
+	}
+}
+
+// isZero reports whether v is the zero value for its type.
+// FIXME IsZero was added to the reflect standard package in go 1.13
+// compatible with older versions of go 1.13
+func isZero(_v interface{}) bool {
+	v := reflect.ValueOf(_v)
+	switch v.Kind() {
+	case reflect.Bool:
+		return !v.Bool()
+	case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64:
+		return v.Int() == 0
+	case reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64, reflect.Uintptr:
+		return v.Uint() == 0
+	case reflect.Float32, reflect.Float64:
+		return math.Float64bits(v.Float()) == 0
+	case reflect.Complex64, reflect.Complex128:
+		c := v.Complex()
+		return math.Float64bits(real(c)) == 0 && math.Float64bits(imag(c)) == 0
+	case reflect.Array:
+		for i := 0; i < v.Len(); i++ {
+			if !isZero(v.Index(i)) {
+				return false
+			}
+		}
+		return true
+	case reflect.Chan, reflect.Func, reflect.Interface, reflect.Map, reflect.Ptr, reflect.Slice, reflect.UnsafePointer:
+		return v.IsNil()
+	case reflect.String:
+		return v.Len() == 0
+	case reflect.Struct:
+		for i := 0; i < v.NumField(); i++ {
+			if !isZero(v.Field(i)) {
+				return false
+			}
+		}
+		return true
+	default:
+		return true
 	}
 }
 
