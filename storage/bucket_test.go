d117bad5d2617738e8cca08c3c0dee41ae3e1300
YangSen-qn <yangsen@qiniu.com>
2022-04-24 15:41:44 +0800

    change stat api test case

--- storage/bucket_test.go
+++ storage/bucket_test.go
@@ -172,8 +172,7 @@ func TestStat(t *testing.T) {
 	}
 	client.DebugMode = true
 	if info, e := bucketManager.Stat(testBucket, copyKey); e != nil ||
-		len(info.Hash) == 0 || len(info.Md5) == 0 || info.Expiration == 0 /* ||
-		info.TransitionToIA == 0 || info.TransitionToArchive == 0 || info.TransitionToDeepArchive == 0 */ {
+		len(info.Hash) == 0 || len(info.Md5) == 0 || info.Expiration == 0 {
 		t.Logf("3 Stat() error, %v", e)
 		t.Fail()
 	} else {
