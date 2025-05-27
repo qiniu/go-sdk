f381ad6ea073723aabc1494fdde45aaa7303e73b
YangSen-qn <yangsen@qiniu.com>
2025-05-27 15:43:51 +0800

    fix: form upload, Specified domain name not effective

--- storagev2/uploader/upload_manager.go
+++ storagev2/uploader/upload_manager.go
@@ -101,7 +101,11 @@ func NewUploadManager(options *UploadManagerOptions) *UploadManager {
 func (uploadManager *UploadManager) UploadDirectory(ctx context.Context, directoryPath string, directoryOptions *DirectoryOptions) error {
 	if directoryOptions == nil {
 		directoryOptions = &DirectoryOptions{}
+	} else {
+		tmp := *directoryOptions
+		directoryOptions = &tmp
 	}
+
 	objectConcurrency := directoryOptions.ObjectConcurrency
 	if objectConcurrency == 0 {
 		objectConcurrency = 4
